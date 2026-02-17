e860e8ff3f8625db4decb6f04b28a105a1b1e35a
unknown <tayyababdullah29@gmail.com>
2026-02-17 23:28:37 +0500

    keys

--- backend/start.py
+++ backend/start.py
@@ -2,7 +2,7 @@ import os
 import uvicorn
 from main import app
 
-port = int(os.environ.get("PORT", 8081))  # Cloud Run sets PORT
+port = int(os.environ.get("PORT", 8080))  # Cloud Run sets PORT
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=port)
